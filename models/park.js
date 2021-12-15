827407fde183fe38c484f25fc3cd6276cdb1fa4e
CodeClan Student <7378035+NodeToNowhere@users.noreply.github.com>
2021-12-15 08:59:13 +0000

    Almost done

---
+++ models/park.js
@@ -0,0 +1,8 @@
+const Park = function () {
+    this.name = "Totally Safe Dino Park";
+    this.ticketPrice = 50.00;
+    this.dinosaurs = []
+}
+
+module.exports = Park;
+
--- specs/park_spec.js
+++ specs/park_spec.js
@@ -4,23 +4,80 @@ const Dinosaur = require('../models/dinosaur.js');
 
 describe('Park', function() {
 
+  let park;
+  let tyrannosaur;
+  let raptor;
+  let stegosaur;
+  let ornithopod;
+  let raptorTwo;
+
   beforeEach(function () {
+    park = new Park()
+    tyrannosaur = new Dinosaur("tyrannosaur", "carnivore", 1234);
+    raptor = new Dinosaur("raptor", "omnivore", 321);
+    stegosaur = new Dinosaur("stegosaur", "herbivore", 12);
+    ornithopod = new Dinosaur("ornithopod", "omnivore", 444);
+    raptorTwo = new Dinosaur("raptor", "omnivore", 456);
+    park.dinosaurs = [tyrannosaur, raptor, stegosaur, raptorTwo];
+    list = [tyrannosaur, raptor, stegosaur, raptorTwo];
+    
+  });
 
-  })
+  it('should have a name', function() {
+    const actual = park.name;
+    assert.strictEqual(actual, 'Totally Safe Dino Park');
+  });
 
-  it('should have a name');
 
-  it('should have a ticket price');
+  it('should have a ticket price', function () {
+    const actual = park.ticketPrice;
+    assert.strictEqual(actual, 50); 
+  });
 
-  it('should have a collection of dinosaurs');
 
-  it('should be able to add a dinosaur to its collection');
+  it('should have a collection of dinosaurs', function () {
+    const actual = park.dinosaurs;
+    assert(actual, actual.length > 0);
+    const actual2 = park.dinosaurs.toString();
+    assert(actual2, list);
+  });
 
-  it('should be able to remove a dinosaur from its collection');
+  it('should be able to add a dinosaur to its collection', function() {
+    const actual = park.dinosaurs.push(ornithopod).toString();
+    list = list.push(ornithopod).toString();
+    assert.equal(actual, list);
+  });
 
-  it('should be able to find the dinosaur that attracts the most visitors');
+  it('should be able to remove a dinosaur from its collection', function() {
+    const actual = park.dinosaurs.filter(x => x !== ornithopod).toString();
+    assert.equal(actual, list.toString())
+  });
 
-  it('should be able to find all dinosaurs of a particular species');
+  it('should be able to find the dinosaur that attracts the most visitors', function () {
+    let none = 0;
+    let dinosaur = '';
+    for (const dino of park.dinosaurs){
+      if (dino.guestsAttractedPerDay > none){
+        none = dino.guestsAttractedPerDay;
+        dinosaur = dino;
+      };
+    };
+    const actual = dinosaur;
+    assert.deepEqual(actual, tyrannosaur);
+  });
+
+  it('should be able to find all dinosaurs of a particular species', function(){
+    let speciesFind = 'raptor';
+    let newList = [];
+    for (const dino of park.dinosaurs){
+      if (dino.species === speciesFind){
+        newList.push(dino)
+      };
+    };
+    const actual = newList.toString();
+    test = [raptor, raptorTwo];
+    assert.strictEqual(actual, test.toString());
+  });
 
   it('should be able to calculate the total number of visitors per day');
 
